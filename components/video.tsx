e617efac0b7900e1f95e083f50fa9a0faa141fe3
Dela <ernestanyomitse752@gmail.com>
2023-03-12 14:39:34 +0000

    cache

--- components/video.tsx
+++ components/video.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const VideoPlayer: React.FC = () => {
+  const videoUrl =
+    "https://res.cloudinary.com/dpbuwo8ha/video/upload/v1678626891/video_zuoi1x.webm";
+
+  // Add the `cache` transformation parameter to the video URL
+  const cachedVideoUrl = `${videoUrl}?cache=31536000`;
+
   return (
     <div className="relative md:w-[60%] w-[100%]">
-      <video
-        controls
-        src="https://res.cloudinary.com/dpbuwo8ha/video/upload/v1678626891/video_zuoi1x.webm"
-      ></video>
+      <video controls src={cachedVideoUrl}></video>
     </div>
   );
 };
