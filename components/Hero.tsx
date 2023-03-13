026cccd729a87e55d4437c77ac03dc23cbe7b9ec
Dela <ernestanyomitse752@gmail.com>
2023-03-13 08:42:52 +0000

    play

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,11 +16,11 @@ export default function Hero({}: Props) {
   return (
     <div className="flex items-center h-screen absolute top-[0] left-[0] w-[100%] justify-center md:px-10 px-5">
       <div className="">
-        <h1 className="text-purple-500 text-4xl font-font xl:text-8xl md:text-5xl font-extrabold  ">
+        <h1 className="text-purple-500 text-4xl font-font xl:text-7xl 2xl:text-8xl md:text-5xl font-extrabold  ">
           We <span className="text-white">{text}</span>
           <Cursor cursorColor="#7B2A93" />
         </h1>
-        <h1 className="text-white text-xl font-font xl:text-4xl md:text-2xl  pt-10">
+        <h1 className="text-white text-xl font-font xl:text-2xl md:text-2xl 2xl:text-4xl  pt-10 xl:pt-5 2xl:pt-10">
           Transformed Africa through creative and responsive leaders.
         </h1>
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import { AppProps } from "next/app";
 import { AnimatePresence, motion } from "framer-motion";
 import "../styles/globals.css";
-import "@fontsource/poppins";
+import "@fontsource/poppins/900.css";
+import "@fontsource/poppins/800.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/100.css";
+
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <AnimatePresence exitBeforeEnter>
